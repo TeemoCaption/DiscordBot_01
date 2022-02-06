e45977a01dc8400e60013aad02bf7882471bd399
TeemoCaption <84553635+TeemoCaption@users.noreply.github.com>
2022-02-06 23:15:13 +0800

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+bot=commands.Bot(command_prefix="!")   #建置Discord機器人
+#command_prefix=>打指令前的命令字首
+
+@bot.event     #機器人事件
+#async def=>協程函式
+async def on_ready():     #功能
+    print("Bot is online")
+
+
+bot.run("OTM5ODI0NzQ4NzY0OTg3Mzk1.Yf-eAQ.GMU0Ues5-K8v_9JUmYHHRQAXIPM")  #執行機器人
