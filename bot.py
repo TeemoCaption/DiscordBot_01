f1cf95ed51c9c93872f1c1904047c7a72d6e7bfa
TeemoCaption <84553635+TeemoCaption@users.noreply.github.com>
2022-02-06 23:42:37 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,15 @@ bot=commands.Bot(command_prefix="!")   #建置Discord機器人
 
 @bot.event     #機器人事件
 #async def=>協程函式
-async def on_ready():     #功能
+async def on_ready():   
     print("Bot is online")
+    
+@bot.event  
+async def on_member_join(member):     #成員加入
+    print(f"{member} 跳進來了！")
+    
+async def on_member_remove(member):     #成員離開
+    print(f"{member} 離開了！")
 
 
-bot.run("OTM5ODI0NzQ4NzY0OTg3Mzk1.Yf-eAQ.JuYXGuSG1HHjVq3DjgY-v2qyKGY")  #執行機器人
+bot.run("OTM5ODI0NzQ4NzY0OTg3Mzk1.Yf-eAQ.9RxcH8jhR_PYByZgUIqmUBbw0u0")  #執行機器人
